ca7becfd6a371f1ca3045d4be6366066ff52fd8a
checksonhk <jackson.to@gmail.com>
2020-01-22 12:53:01 -0500

    add seed data, added status and due_by to task schema

--- db/schema/schema.sql
+++ db/schema/schema.sql
@@ -1,17 +1,22 @@
 DROP TABLE tasks;
 
+
 CREATE TABLE IF NOT EXIST users (
   id SERIAL PRIMARY KEY,
   acc VARCHAR(20) UNIQUE, -- use sessions to indentify unique visitors 
 )
+
+CREATE TYPE STATUS as ENUM('active','pending', 'done');
 
 
 CREATE TABLE IF NOT EXIST tasks (
   id SERIAL PRIMARY KEY,
   -- user_id INTEGER REFERENCES users(id) NOT NULL ON DELETE CASCADE,
   title VARCHAR(255) NOT NULL,
+  status STATUS DEFAULT 'active',
   active BOOLEAN NOT NULL DEFAULT TRUE,
-  created_at TIMESTAMP NOT NULL DEFAULT NOW();
+  created_at TIMESTAMP NOT NULL DEFAULT NOW()
+  due_by TIMESTAMP NOT NULL
   -- category_id
 )
 
@@ -25,5 +30,5 @@ CREATE TABLE IF NOT EXIST categories (
 --   task_id INTEGER REFERENCES tasks(id) NOT NULL ON DELETE CASCADE,
 --   title VARCHAR(255) NOT NULL,
 --   active BOOLEAN NOT NULL DEFAULT TRUE,
---   created_at TIMESTAMP NOT NULL DEFAULT NOW();
+--   created_at TIMESTAMP NOT NULL DEFAULT NOW()
 -- )
---
+++ db/seeds/tasks.sql
@@ -0,0 +1,7 @@
+INSERT INTO tasks (title, due_by) VALUES 
+  ('Be A Kind Person', '2020-02-16 20:09:43'),
+  ('Learn Programming!', '2020-02-16 20:09:43'),
+  ('Improve Yourself', '2020-02-16 20:09:43'),
+  ('Finish the Homework', '2020-02-16 20:09:43'),
+  ('Pure Your Thoughts', '2020-02-16 20:09:43'),
+  ('Clean Your Heart!', '2020-02-16 20:09:43');
