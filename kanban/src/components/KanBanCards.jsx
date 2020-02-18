a2d324bb02c7b7a44b3a4e0cb3170b14d62d7d46
checksonhk <jackson.to@gmail.com>
2020-02-17 21:42:57 -0500

    implement KanbanCard

--- kanban/src/components/KanBanCards.jsx
+++ kanban/src/components/KanBanCards.jsx
@@ -1,50 +1,44 @@
+import React from 'react';
 /*
  * The Kanban Board Card component
  */
-class KanbanCard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      collapsed: true,
-    };
-  }
+export default function KanbanCard(props) {
+  const [collapsed, setCollapsed] = useState(true);
 
-  render() {
-    const cardStyle = {
-      backgroundColor: '#f9f7f7',
-      paddingLeft: '0px',
-      paddingTop: '5px',
-      paddingBottom: '5px',
-      marginLeft: '0px',
-      marginRight: '5px',
-      marginBottom: '5px',
-    };
+  const cardStyle = {
+    backgroundColor: '#f9f7f7',
+    paddingLeft: '0px',
+    paddingTop: '5px',
+    paddingBottom: '5px',
+    marginLeft: '0px',
+    marginRight: '5px',
+    marginBottom: '5px',
+  };
 
-    return (
-      <div
-        style={cardStyle}
-        draggable={true}
-        onDragEnd={e => {
-          this.props.onDragEnd(e, this.props.project);
-        }}>
-        <div>
-          <h4>{this.props.project.name}</h4>
-        </div>
-        {this.state.collapsed ? null : (
-          <div>
-            <strong>Description: </strong>
-            {this.props.project.description}
-            <br />
-          </div>
-        )}
-        <div
-          style={{ width: '100%' }}
-          onClick={e => {
-            this.setState({ collapsed: !this.state.collapsed });
-          }}>
-          {this.state.collapsed ? String.fromCharCode('9660') : String.fromCharCode('9650')}
-        </div>
+  return (
+    <div
+      style={cardStyle}
+      draggable={true}
+      onDragEnd={e => {
+        props.onDragEnd(e, props.project);
+      }}>
+      <div>
+        <h4>{props.project.name}</h4>
       </div>
-    );
-  }
+      {!collapsed && (
+        <div>
+          <strong>Description: </strong>
+          {props.project.description}
+          <br />
+        </div>
+      )}
+      <div
+        style={{ width: '100%' }}
+        onClick={e => {
+          setCollapsed(state => !state);
+        }}>
+        {collapsed ? String.fromCharCode('9660') : String.fromCharCode('9650')}
+      </div>
+    </div>
+  );
 }
